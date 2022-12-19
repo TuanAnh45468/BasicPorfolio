bf7fd6d8394b44b946f7d7c9572b0fc89fcca430
Hoang Tuan Anh <tuananh45468@gmail.com>
2022-12-19 17:51:17 +0700

    complete about section

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
+import About from "./components/About/About";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
+import Nav from "./components/Header/Nav";
+import Home from "./components/HomeComponent/Home";
 
 function App() {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+  return (
+    <div className='grid h-screen w-screen grid-cols-12 bg-zinc-100'>
+      <Nav className='col-span-2 col-end-2 h-screen w-60 bg-zinc-200 text-xl'></Nav>
+      <Home className='col-span-8 col-start-3 row-span-1 mt-24 h-screen'></Home>
+      <About className='col-span-8 col-start-4 mt-24 h-screen'>
+        About section
+      </About>
+    </div>
+  );
 }
 
 export default App;
--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,7 +1,57 @@
 import React from "react";
+import profileImage1 from "../../img/profileImage1.jpg";
+import Button from "../UI/Button";
+import { BiMedal } from "react-icons/bi";
+import { BiPackage } from "react-icons/bi";
+import { BiHeadphone } from "react-icons/bi";
 
-const About = () => {
-  return <div>About</div>;
+const About = (props) => {
+  return (
+    <section className={props.className + " grid gap-4 "}>
+      <div className='self-center'>
+        <h1 className='text-center text-5xl font-medium text-zinc-800'>
+          About me
+        </h1>
+      </div>
+      <div className='grid grid-cols-2 justify-center'>
+        <figure>
+          <img
+            src={profileImage1}
+            alt='profile'
+            className='col-span-1 h-56 w-56 items-center rounded-md object-cover shadow-xl grayscale'
+          ></img>
+        </figure>
+
+        <div className='col-span-1'>
+          <div className='mb-8 flex gap-8'>
+            <div className='flex flex-col items-start justify-center'>
+              <BiMedal className='h-7 w-7 fill-zinc-800 '></BiMedal>
+              <p>Experience</p>
+              <p>1 + year</p>
+            </div>
+
+            <div className='flex flex-col items-start justify-center'>
+              <BiPackage className='h-7 w-7 fill-zinc-800'></BiPackage>
+              <p>Completed</p>
+              <p>3 + projects</p>
+            </div>
+
+            <div className='flex flex-col items-start justify-center'>
+              <BiHeadphone className='h-7 w-7 fill-zinc-800'></BiHeadphone>
+              <p>Support</p>
+              <p>Office Hours</p>
+            </div>
+          </div>
+          <p className='mb-8'>
+            I am a beginner front-end developer and college student
+          </p>
+          <Button className='w-auto rounded-md bg-zinc-700 px-5 text-white hover:bg-zinc-600'>
+            Download CV
+          </Button>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
