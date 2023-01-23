9c079e4dac8fd2514a56e7fdf7b7f13d6119015c
Hoang Tuan Anh <tuananh45468@gmail.com>
2023-01-23 23:41:23 +0700

    create bulid folder

---
+++ build/precache-manifest.b0e4dc5f6235d527087d98660a3caac0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "301d55f953e04ec44e08",
+    "url": "/static/css/main.b703abf4.chunk.css"
+  },
+  {
+    "revision": "301d55f953e04ec44e08",
+    "url": "/static/js/main.301d55f9.chunk.js"
+  },
+  {
+    "revision": "3f5a8f4c7bc0532f173e",
+    "url": "/static/js/1.3f5a8f4c.chunk.js"
+  },
+  {
+    "revision": "e9ef0edb4f754fc3999a",
+    "url": "/static/js/2.e9ef0edb.chunk.js"
+  },
+  {
+    "revision": "38973534419a972cf74f",
+    "url": "/static/js/runtime~main.38973534.js"
+  },
+  {
+    "revision": "f041830d14cf5caa24f1c33627bf8244",
+    "url": "/static/media/profileImage1.f041830d.jpg"
+  },
+  {
+    "revision": "0df2d24bf6755cc5f25b5a6b2c217af4",
+    "url": "/index.html"
+  }
+];
